f20cb3ea45416e5e5e5b469c9bf8b60d80d02473
Ilya Soloveychik <dj.active.17@gmail.com>
2023-08-11 16:55:08 +0700

    init

---
+++ client.js
@@ -0,0 +1,44 @@
+const DATA = {};
+
+DATA.generate_period = 3;
+DATA.send_period = 4;
+DATA.generate_period_2 = 2;
+DATA.send_period_2 = 3;
+
+const delay = (delaySec) => {
+   return new Promise(resolve => setTimeout(resolve, delaySec * 1000));
+}
+
+
+const myWs = new WebSocket('ws://localhost:9000');
+myWs.onopen = function () {
+    console.log('I am connected');
+};
+
+const wsSendCoordinates = async (e) => {
+    await delay(DATA.generate_period);
+    let interval;
+
+    if (interval) clearTimeout(interval)
+
+    interval = setInterval(() => {
+        myWs.send(JSON.stringify({action: 'COORD', data: ''}));
+    }, DATA.send_period * 1000);
+}
+
+const workerCPUEvent = async () => {
+    await delay(DATA.generate_period_2);
+    let interval;
+
+    if (interval) clearTimeout(interval)
+
+    interval = setInterval(() => {
+        myWs.send(JSON.stringify({action: 'CPU', data: 'go'}));
+    }, DATA.send_period * 1000);
+
+}
+
+
+
+
+document.addEventListener("mousemove", wsSendCoordinates, false);
